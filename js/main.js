91e8a70a184d45bb1087a07e4f04eaed5117a1ab
MissDitch <mjtsloot@icloud.com>
2017-02-17 10:45:05 +0100

    contact form label change: js instead of css

--- js/main.js
+++ js/main.js
@@ -10,17 +10,21 @@ $(document).ready(function() {
 function showLabel(e) {
 	var label = $("label[for='"+$(this).attr('id')+"']");
 	label.show("slow");
+		$(this).addClass("white-placeholder" );
 }
 
 function clearedInput() {
     if (!this.value) {
 			var label = $("label[for='"+$(this).attr('id')+"']");
 			label.hide("slow");
+			$(this).removeClass("white-placeholder" );
 		}	
 }
 function showMessage(e) {	
 		e.preventDefault();	
 		$(".control-label").hide();
-		var n =	$("#notify").append('<p>form is not really submitted</p>');
-n.fadeOut(5000, function() {	$(".form-control").val(""); });
+		var n =	$("#notify").append('<p>Don\'t worry, form is not really submitted!</p>');
+n.fadeOut(5000 );
+	$(".form-control").val("");
+	$(".form-control").removeClass("white-placeholder");
 }
