c77aade6591e432674a9038286e0b0233ca40580
MissDitch <mjtsloot@icloud.com>
2017-02-17 14:04:22 +0100

    navigation, css structuring, media queries

--- js/main.js
+++ js/main.js
@@ -3,9 +3,34 @@ $(document).ready(function() {
 	$(".form-control").focus(showLabel);
 	$(".form-control").keyup(clearedInput);
 	$("#submitBtn").click(showMessage);
+	$("li").click(function() {
+      	// remove classes from all
+    	$("li").removeClass("active");
+      	// add class to the one we clicked
+      	$(this).addClass("active");
+  	});
+	   // run test on initial page load
+    checkSize();
+
+    // run test on resize of the window
+    $(window).resize(checkSize);
 	
 	
 })
+
+
+//Function to the css rule
+function checkSize(){
+    if ($(".name").css("white-space") == "nowrap" ){
+       $("h3 span").removeClass("text-nowrap");
+    }
+	else {
+		 $("h3 span").addClass("text-nowrap");
+	}
+}
+
+
+
 
 function showLabel(e) {
 	var label = $("label[for='"+$(this).attr('id')+"']");
