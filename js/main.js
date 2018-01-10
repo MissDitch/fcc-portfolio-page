7d268c2a356ed346208ccc2d809b98daafd92ccf
Marian Sloot <mjtsloot@icloud.com>
2018-01-10 11:09:56 +0100

    design changes, change active state of navigation items on scroll

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ function checkSize(){
 	else {
 		 $("h3 span").addClass("text-nowrap");
 	}
-}
 
+}
 
 
 
@@ -53,3 +53,53 @@ n.fadeOut(5000 );
 	$(".form-control").val("");
 	$(".form-control").removeClass("white-placeholder");
 }
+
+
+// http://codetheory.in/change-active-state-links-sticky-navigation-scroll/
+
+var sections = $('section')
+  , nav = $('#myNavbar')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+// from	jquery-scroll-bottom.js
+	var scrollHeight = $(document).height();
+var scrollPosition = $(window).height() + $(window).scrollTop();
+// 	end code jquery-scroll-bottom.js
+	
+// // https://gist.github.com/toshimaru/6102647	
+// $(window).on("scroll", function() {
+// 	var scrollHeight = $(document).height();
+// 	var scrollPosition = $(window).height() + $(window).scrollTop();
+// 	if ((scrollHeight - scrollPosition) / scrollHeight === 0) {
+// 	    // when scroll to bottom of the page
+// 	}
+// });
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom || 
+				// when scoll to bottom of page:
+			 (scrollHeight - scrollPosition) / scrollHeight === 0) {
+      nav.find('a').parent().removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
